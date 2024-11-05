2f2ce31d3835e05dc3bd0485324d8409756648ad
Kamdarsel <114313151+Kamdarsel@users.noreply.github.com>
2024-11-05 01:22:01 -0500

    Arreglos finales interfaces

--- Bio-Find/Assets/Project/Controlador/Scripts2/CanvaPause.cs
+++ Bio-Find/Assets/Project/Controlador/Scripts2/CanvaPause.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR; // Asegúrate de importar el espacio de nombres XR
 
 public class CanvaPause : MonoBehaviour
 {
     public GameObject CanvaPausa;
+    private bool Cambio;
 
-    bool Cambio;
-
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        // Obtener el estado de los dispositivos XR
+        InputDevice leftController = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+
+        // Verifica si el controlador izquierdo está conectado
+        if (leftController.isValid)
         {
-            Cambio = !Cambio;
-            CanvaPausa.SetActive(Cambio);
+            // Verifica si el botón izquierdo (Primary Button) está presionado
+            if (leftController.TryGetFeatureValue(CommonUsages.primaryButton, out bool isPressed) && isPressed)
+            {
+                // Cambia el estado del Canvas solo si se presiona el botón
+                Cambio = !Cambio;
+                CanvaPausa.SetActive(Cambio);
+            }
         }
     }
 }
